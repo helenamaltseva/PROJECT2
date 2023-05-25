98ad34c461cb24a7e818deb5c57ec785d6a358e2
helenamaltseva <helena.maltseva@gmail.com>
2023-05-25 12:11:06 +0300

    Changed files

--- js/mapInit.js
+++ js/mapInit.js
@@ -1,14 +1,37 @@
-function initMap() {
-   const uluru = { lat: -25.344, lng: 131.031 };
-    const map = new google.maps.Map(document.getElementById("map"), {
+// function initMap() {
+//    const uluru = { lat: -25.344, lng: 131.031 };
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: uluru,
+//     mapID: "df0d5421b2d94e45"
+//   });
+//   new google.maps.Marker({
+//     position: uluru,
+//     map: map,
+//   });
+// }
+
+// initMap();
+
+let map;
+
+async function initMap() {
+  const position = { lat: -25.344, lng: 131.031 };
+
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
     zoom: 4,
-    center: uluru,
-    mapID: "df0d5421b2d94e45"
+    center: position,
+    mapId: "df0d5421b2d94e45",
   });
-  new google.maps.Marker({
-    position: uluru,
+
+  const marker = new AdvancedMarkerElement({
     map: map,
+    position: position,
+    title: "Uluru",
   });
 }
 
-window.initMap = initMap;
+initMap();
